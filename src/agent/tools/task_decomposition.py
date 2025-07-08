140a160bc71793b37416a173e9552f866e407163
Jonathan <jonathanmhtran@gmail.com>
2025-07-08 03:23:59 -0700

    Prompt engineered task decomposition model for more concise and consistent responses

--- src/agent/tools/task_decomposition.py
+++ src/agent/tools/task_decomposition.py
@@ -1,33 +1,64 @@
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableSequence
-from langchain_openai import OpenAI
+from langchain_core.runnables import RunnableSequence, RunnableLambda
 
-from agent.tools.wrapper import ToolModelWrapper
+from agent.tools.wrapper import ModelWrapper
+import agent.planner as planner
 
-class TaskDecompositionModel(ToolModelWrapper):
+import regex as re
+
+class TaskDecompositionModel(ModelWrapper):
     def __init__(self, system_prompt, model, **kwargs):
         self.input_template = lambda x: {
             "input": x["input"]
         }
-        self.parse_func = lambda x: x
+        self.parse_func = parse_decomp_output
 
         super().__init__(
+            agent_type="task_decomposition",
             system_prompt=system_prompt,
             input_template=self.input_template,
-            model_name=model,
+            model=model,
             parse_func=self.parse_func,
             **kwargs
         )
+        
+        self.planner_model = planner.PlannerModel(
+            system_prompt=planner.default_planner_prompt(),
+            model_name=kwargs.get("model", "mistral:7b"),
+            temperature=kwargs.get("temperature", 0.1),
+            num_ctx=kwargs.get("max_tokens", 20000),
+        )
+        
+        self.chain = self.chain | RunnableLambda(lambda x: self.prepare_inputs_for_planner(x)) \
+            | RunnableLambda(lambda x: list(map(self.planner_model.invoke, x)))
+            
+    def prepare_inputs_for_planner(self, x: list[str]):
+        return [{"input": i} for i in x]
+
+def parse_decomp_output(output):
+    """
+    Parses the output from the task decomposition model.
+    
+    Args:
+        output (str): The output string from the model.
+        
+    Returns:
+        list: A list of sub-questions extracted from the output.
+    """
+    output_lines = output.content.strip().split("\n")
+    matches = [re.match(r"^(\d+\. )(.*)", line) for line in output_lines]
+    return [match.group(2) for match in matches if match]
 
 
-decomp_prompt = """INSTRUCTIONS
-You are a domain expert. Your task is to break down a complex task into simpler sub-parts
-
-USER QUESTION
-{{user_question}}
+decomp_prompt = """
+INSTRUCTIONS
+You are a domain expert. Your task is to break down a complex task into simpler sub-parts.
+Given a user question, decompose it into a brief list of sub-questions that can be answered independently.
 
 ANSWER FORMAT
 Answer in a numbered list of sub-questions
+Do not add any additional information, just the list of sub-questions.
+Do not use any special characters or formatting.
 
 EXAMPLE
 question: Which country is larger in population: France or Germany?
@@ -38,28 +69,39 @@ answer:
 
 question: How do I get a software engineering job as a student?
 answer:
-1. How do I improve my skills as a programmer?
-2. How can I grow my professional network?
-3. What kinds of projects are good for my portfolio?
-4. How can I prepare for interviews?
-5. How can I make myself stand out from other candidates?
+1. How can I make myself a competitive candidate?
+2. What are the best resources for making myself a competitive candidate?
 
-question: How do I get started with reinforcement learning research?
+question: How has cancer research evolved over the past decade, and what are the current trends in treatment and prevention?
 answer:
-1. What are the foundational papers in reinforcement learning?
-2. What are the current trends in reinforcement learning research?
-3. What are recent papers in reinforcement learning?
+1. What are some major advancements in cancer research over the past decade?
+2. What are the current trends in cancer treatment and prevention?
+
+question: What is the history of quantum computing, and how does it relate to modern research in the field?
+answer:
+1. What is the history of quantum computing?
+2. What does modern research in quantum computing focus on?
+3. Are there any differences between the history of quantum computing and modern research?
+
+question:
+
+Use as few sub-questions as possible. Keep the sub-questions concise and focused on the main aspects of the task.
+Do not include any additional information or explanations in your response.
 """
-decomp_template = ChatPromptTemplate.from_messages(
-    [
-        ("system", decomp_prompt),
-        ("human", "{user_question}")
-    ]
-)
-planner_llm = OpenAI(temperature=0)
-planner_chain = RunnableSequence(
-    {
-        "user_question": lambda x: x["question"],
-    },
-    decomp_template | planner_llm
-)
+
+if __name__ == "__main__":
+    # Example usage
+    model = TaskDecompositionModel(system_prompt=decomp_prompt, model="llama3.2:3b", 
+                                   temperature=0.1, num_ctx=1000, streaming=True)
+    print()
+    while True:
+        user_input = input(">>>  ")
+        if user_input.lower() in ["exit", "quit"]:
+            break
+        input_data = {"input": user_input}
+        output = model.invoke(input_data)
+        print()
+    # input_data = {"input": "How does computer vision research relate to artificial intelligence?"}
+    # output = model.invoke(input_data)
+    # for result in [x.content.strip() for x in output]:
+    #     print(result)
