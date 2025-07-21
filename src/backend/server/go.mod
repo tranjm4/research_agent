afe835442c579fcee1b4c0a00c4eb7054964c5f0
Jonathan <jonathanmhtran@gmail.com>
2025-07-21 01:20:23 -0700

    Partial implementation of Go backend server for user auth system (todo: conversations) (will serve as proxy to LLM server)

---
+++ src/backend/server/go.mod
@@ -0,0 +1,21 @@
+module server
+
+go 1.24.5
+
+require github.com/go-chi/chi/v5 v5.2.2
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ src/backend/server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"fmt"
+	"log"
+	"time"
+
+	"server/routes/userauth"
+	"server/utils"
+
+	"database/sql"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/jackc/pgx/v5/stdlib" // Import the PostgreSQL driver
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set timeout for requests after 10 minutes
+	r.Use(middleware.Timeout(10 * time.Minute))
+
+	// Initialize the database connection
+	// Connect to the PostgreSQL database
+	dsn := utils.GetDBUrl(".env")
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	defer db.Close() // Cleanup function when main exits
+
+	// Ping the database to ensure connection is established
+	ctx := context.Background()
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatalf("failed to ping DB %v", err)
+	}
+
+	// Register routes
+	userauth.RegisterRoutes(r, db)
+	// chat.RegisterRoutes(r, db)
+	// ai.RegisterRoutes(r, db)
+
+	fmt.Println("Connected to PostgreSQL database")
+	fmt.Println("Server is running on", utils.GetServerUrl(".env"))
+	http.ListenAndServe(":8080", r)
+}
